f2017f6b62d0d3ccc4d033738ad562619dcae8a7
poqopo <block.poqopo@gmail.com>
2022-09-21 15:44:52 +0900

    modify Input

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,7 +3,7 @@ import styles from "../assets/Input.module.css";
 
 function Input({ placeholder, onChange }) {
   return (
-    <input               type="number" className={styles.Input} placeholder={placeholder} onChange={onChange}/>
+    <input className={styles.Input} placeholder={placeholder} onChange={onChange}/>
   );
 }
 export default Input;
